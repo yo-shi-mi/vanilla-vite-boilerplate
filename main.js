6ae5f649d9f7091d392971c26018ad119ba23007
Erin <erin001tw@gmail.com>
2024-06-28 22:29:11 +0800

    add logo

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
+import logoSrc from './assets/images/logo.png'
 
 document.querySelector('#app').innerHTML = `
   <div>
