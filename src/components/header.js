7d004f95226c75b97f7b541658bec067103208a1
Erin <erin001tw@gmail.com>
2024-06-28 23:43:58 +0800

    connect wallet button

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,14 @@
 export function renderHeader() {
+  const isTWA = window.Telegram && window.Telegram.WebApp;
+
   const header = document.createElement('header')
   header.innerHTML = `
     <div class="container header-content">
       <div class="logo">
-        <img src="./images/logo.jpg" alt="TeleDine Logo">
+        <img src="./images/logo.png" alt="TeleDine Logo">
         <h1>TeleDine</h1>
       </div>
-      ${!window.Telegram?.WebApp ? '<button id="connectWallet" class="connect-wallet">Connect Wallet</button>' : ''}
+      ${!isTWA ? '<button id="connectWallet" class="connect-wallet">Connect Wallet</button>' : ''}
     </div>
   `
   return header
